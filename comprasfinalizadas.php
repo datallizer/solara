1ab2d7c9dbe44e52305ecd751d699af46e59dc96
David Aguilar <utm19040003@utma.edu.mx>
2024-09-11 23:13:10 -0600

    AlmacenadoEnServerYNotificacionesSinAsignacionV7

--- comprasfinalizadas.php
+++ comprasfinalizadas.php
@@ -28,7 +28,7 @@ if (isset($_SESSION['codigo'])) {
     $query = "SELECT usuarios.codigo, usuarios.estatus FROM usuarios WHERE codigo = '$codigo' AND estatus = 1";
     $result = mysqli_query($con, $query);
     if (mysqli_num_rows($result) > 0) {
-        $queryubicacion = "UPDATE `usuarios` SET `ubicacion` = 'Compras' WHERE `usuarios`.`codigo` = '$codigo'";
+        $queryubicacion = "UPDATE `usuarios` SET `ubicacion` = 'Compras finalizadas' WHERE `usuarios`.`codigo` = '$codigo'";
         $queryubicacion_run = mysqli_query($con, $queryubicacion);
     } else {
         header('Location: login.php');
@@ -46,7 +46,7 @@ if (isset($_SESSION['codigo'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Compras | Solara</title>
+    <title>Compras finalizadas | Solara</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
@@ -56,30 +56,31 @@ if (isset($_SESSION['codigo'])) {
 
 <body class="sb-nav-fixed">
     <?php include 'sidenav.php'; ?>
-<?php include 'mensajes.php'; ?>
+    <?php include 'mensajes.php'; ?>
     <div id="layoutSidenav">
         <div id="layoutSidenav_content">
             <div class="container-fluid">
                 <div class="row mb-5 mt-5">
                     <div class="col-md-12 mt-3">
-                    <a href="comprasfinalizadas.php" class="btn btn-primary btn-sm" id="floatingButton">
-                                Compras<br>finalizadas
-                            </a>
+                        <a href="compras.php" class="btn btn-primary btn-sm" id="floatingButton">
+                            Compras<br>pendientes
+                        </a>
                         <div class="card">
                             <div class="card-header">
-                                <h4>COMPRAS PENDIENTES</h4>
+                                <h4><button id="generatePDF" class="btn btn-primary float-end">Generar PDF</button>
+                                    COMPRAS FINALIZADAS</h4>
                             </div>
                             <div class="card-body" style="overflow-y:scroll;">
-                                <table id="miTabla" class="table table-bordered table-striped" style="width: 100%;">
+                                <table id="miTablaDos" class="table table-bordered table-striped" style="width: 100%;">
                                     <thead>
                                         <tr>
+                                            <th></th>
                                             <th>#</th>
                                             <th>Solicitante</th>
                                             <th>Rol</th>
                                             <th>Proyecto</th>
                                             <th>PDF</th>
                                             <th>Notas</th>
-                                            <th>Estatus</th>
                                             <th>Monto</th>
                                             <?php
                                             if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], [1, 2, 6])) {
@@ -93,7 +94,7 @@ if (isset($_SESSION['codigo'])) {
                                         $query = "SELECT quotes.*, proyecto.*, quotes.id AS id_quote
                                             FROM quotes 
                                             JOIN proyecto ON quotes.proyecto = proyecto.id
-                                            WHERE estatusq = 0 OR estatusq = 7
+                                            WHERE estatusq = 2
                                             ORDER BY quotes.id ASC";
 
 
@@ -102,6 +103,9 @@ if (isset($_SESSION['codigo'])) {
                                             foreach ($query_run as $registro) {
                                         ?>
                                                 <tr>
+                                                    <td>
+                                                        <input type="checkbox" class="select-row" data-id="<?= $registro['id_quote']; ?>" data-nombre="<?= $registro['nombre']; ?>" data-cotizacion="<?= $registro['cotizacion']; ?>" data-monto="<?= $registro['monto']; ?>" data-nota="<?= $registro['notas']; ?>">
+                                                    </td>
                                                     <td><?= $registro['id_quote']; ?></td>
                                                     <td><?= $registro['solicitante']; ?></td>
                                                     <td>
@@ -134,34 +138,44 @@ if (isset($_SESSION['codigo'])) {
                                                         <a href="vercompra.php?id=<?= $registro['id_quote']; ?>" class="btn btn-outline-dark btn-sm">Cotizacion <?= $registro['cotizacion']; ?></a>
                                                     </td>
                                                     <td><?= $registro['notas']; ?></td>
-                                                    <td><?php
-                                                        if ($registro['estatusq'] === '0') {
-                                                            echo "Aprobado";
-                                                        } elseif ($registro['estatusq'] === '1') {
-                                                            echo "Pendiente";
-                                                        } elseif ($registro['estatusq'] === '2') {
-                                                            echo "Completada";
-                                                        } elseif ($registro['estatusq'] === '7') {
-                                                            echo "Compra parcial";
-                                                        } else {
-                                                            echo "Error, contacte a soporte";
-                                                        }
-                                                        ?>
-                                                    </td>
                                                     <td>$<?= $registro['monto']; ?></td>
 
                                                     <?php
                                                     if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], [1, 2, 6])) {
                                                     ?>
                                                         <td>
-                                                            <form action="codequotes.php" method="POST" class="d-inline" id="formCompletar<?= $registro['id_quote']; ?>">
-                                                                <button type="button" onclick="completarCompra(<?= $registro['id_quote']; ?>)" class="btn btn-success btn-sm m-1"><i class="bi bi-box-fill"></i> Completar</button>
-                                                                <input type="hidden" name="completar" value="<?= $registro['id_quote']; ?>">
-                                                                <input type="hidden" id="monto<?= $registro['id_quote']; ?>" name="monto">
-                                                                <input type="hidden" id="estatus<?= $registro['id_quote']; ?>" name="estatus">
+                                                            <form action="codequotes.php" method="POST" class="d-inline">
+                                                                <button type="submit" name="delete" value="<?= $registro['id_quote']; ?>" class="btn btn-danger btn-sm m-1 deletebtn"><i class="bi bi-trash-fill deletebtn"></i></button>
                                                             </form>
+                                                            <button type="button" value="<?= $registro['id_quote']; ?>" class="btn btn-warning btn-sm m-1" data-bs-toggle="modal" data-bs-target="#exampleModalDos<?= $registro['id_quote']; ?>"><i class="bi bi-pencil-square"></i></button>
                                                         </td>
+                                                        <div class="modal fade" id="exampleModalDos<?= $registro['id_quote']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?= $registro['id_quote']; ?>" aria-hidden="true">
+                                                            <div class="modal-dialog">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <h1 class="modal-title fs-5" style="text-transform: uppercase;" id="tituloPlanoDos<?= $registro['id_quote']; ?>">EDITAR <?= $registro['cotizacion']; ?></h1>
+                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        <form action="codequotes.php" method="POST" class="row">
+                                                                            <input type="hidden" name="id" value="<?= $registro['id_quote']; ?>" id="id">
+                                                                            <input type="hidden" class="form-control" name="pasado" id="pasado<?= $registro['id_quote']; ?>" value="<?= $registro['monto']; ?>">
+                                                                            <input type="hidden" class="form-control" name="cotizacion" id="cotizacion<?= $registro['id_quote']; ?>" value="<?= $registro['cotizacion']; ?>">
 
+                                                                            <div class="form-floating col-12 mb-3">
+                                                                                <input type="text" class="form-control" name="monto" id="monto<?= $registro['id_quote']; ?>" placeholder="Monto" value="<?= $registro['monto']; ?>">
+                                                                                <label style="padding-left: 0px;" for="monto<?= $registro['id_quote']; ?>">Monto</label>
+                                                                            </div>
+
+                                                                            <div class="modal-footer">
+                                                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                                                <button type="submit" class="btn btn-primary" name="updatemonto">Guardar</button>
+                                                                            </div>
+                                                                        </form>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     <?php
                                                     }
                                                     ?>
@@ -190,59 +204,134 @@ if (isset($_SESSION['codigo'])) {
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
     <script src='https://cdn.jsdelivr.net/npm/sweetalert2@10'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.13/jspdf.plugin.autotable.min.js"></script>
+
     <script>
-        $(document).ready(function() {
-            $('#miTabla').DataTable({
-                "order": [
-                    [0, "desc"]
-                ],
-                "pageLength": 25
+     $(document).ready(function() {
+    // Inicializamos DataTables
+    const table = $('#miTablaDos').DataTable({
+        "order": [
+            [1, "desc"]
+        ],
+        "pageLength": 50
+    });
+
+    // Variable para saber si ya se ha aplicado el filtro
+    let isFiltered = false;
+
+    // Escuchar el evento de seleccionar un checkbox
+    $(document).on('change', '.select-row', function() {
+        const nombre = $(this).data('nombre');
+        const isChecked = $(this).prop('checked');
+
+        if (isChecked) {
+            // Si el checkbox está marcado, aplicar filtro y seleccionar todas las filas con el mismo nombre
+            if (!isFiltered) {
+                // Aplicar filtro en la búsqueda de DataTables
+                table.search(nombre).draw();
+                isFiltered = true;
+            }
+
+            // Marcar todas las filas que coincidan con el nombre
+            table.rows({ search: 'applied' }).every(function() {
+                const rowNode = this.node();
+                const rowNombre = $(rowNode).find('.select-row').data('nombre');
+
+                // Si el nombre coincide, marcar la fila
+                if (rowNombre === nombre) {
+                    $(rowNode).find('.select-row').prop('checked', true);
+                }
+            });
+        } else {
+            // Si el checkbox está desmarcado, permitir desmarcar la fila
+            if ($('.select-row:checked').length === 0) {
+                // Si no hay más checkboxes seleccionados, restablecer el filtro
+                table.search('').draw();
+                isFiltered = false;
+            }
+        }
+    });
+
+    // Generar el PDF cuando se hace clic en el botón
+    $('#generatePDF').on('click', function() {
+        let selectedRows = [];
+
+        // Recoger todas las filas seleccionadas
+        $('.select-row:checked').each(function() {
+            selectedRows.push({
+                id: $(this).data('id'),
+                nombre: $(this).data('nombre'),
+                cotizacion: $(this).data('cotizacion'),
+                monto: $(this).data('monto'),
+                nota: $(this).data('nota')
             });
         });
 
-        function completarCompra(idQuote) {
-            Swal.fire({
-                title: '¿La compra fue total o parcial?',
-                showDenyButton: true,
-                showCancelButton: true,
-                confirmButtonText: 'Total',
-                denyButtonText: 'Parcial',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire({
-                        title: 'Ingrese la cantidad total',
-                        input: 'text',
-                        inputAttributes: {
-                            min: 0
-                        },
-                        showCancelButton: true,
-                        confirmButtonText: 'Enviar'
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            document.getElementById('monto' + idQuote).value = result.value;
-                            document.getElementById('estatus' + idQuote).value = 2;
-                            document.getElementById('formCompletar' + idQuote).submit();
-                        }
-                    });
-                } else if (result.isDenied) {
-                    Swal.fire({
-                        title: 'Ingrese la cantidad comprada',
-                        input: 'text',
-                        inputAttributes: {
-                            min: 0
-                        },
-                        showCancelButton: true,
-                        confirmButtonText: 'Enviar'
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            document.getElementById('monto' + idQuote).value = result.value;
-                            document.getElementById('estatus' + idQuote).value = 7;
-                            document.getElementById('formCompletar' + idQuote).submit();
-                        }
-                    });
-                }
-            });
+        if (selectedRows.length > 0) {
+            // Llamar a la función para generar el PDF
+            generatePDF(selectedRows);
+        } else {
+            alert('Por favor selecciona al menos una fila.');
         }
+    });
+
+    function generatePDF(rows) {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+
+    // La URL de la imagen o base64 de la imagen (ejemplo de base64)
+    const imgData = 'images/logolateral.png'; // Reemplaza esto con tu imagen base64
+
+    // Añadir la imagen en la parte superior derecha
+    const margin = 10; // Margen desde el borde derecho
+    const imgWidth = 38; // Ancho de la imagen
+    const imgHeight = 15; // Alto de la imagen
+    const pageWidth = doc.internal.pageSize.width; // Ancho de la página
+    doc.addImage(imgData, 'PNG', pageWidth - imgWidth - margin, margin, imgWidth, imgHeight);
+
+    // Título del PDF
+    const nombreUpper = rows.length > 0 ? rows[0].nombre.toUpperCase() : '';
+    doc.text(`COMPRAS ${nombreUpper}`, 10, 30); // Ajusta la posición Y si es necesario
+
+    // Definir las columnas de la tabla
+    const head = [
+        ['#', 'Cotización', 'Notas', 'Monto']
+    ];
+
+    // Definir las filas de la tabla con los datos seleccionados
+    const body = rows.map((row, index) => [
+        index + 1,
+        row.cotizacion,
+        row.nota,
+        `$${parseFloat(row.monto || 0).toFixed(2)}` // Manejar valores vacíos o indefinidos
+    ]);
+
+    // Calcular el total de los montos
+    const totalMonto = rows.reduce((total, row) => {
+        const monto = typeof row.monto === 'string'
+            ? parseFloat(row.monto.replace(/[$,]/g, '')) || 0
+            : parseFloat(row.monto) || 0;
+        
+        return total + monto;
+    }, 0);
+
+    // Añadir una fila de total al final de la tabla
+    body.push(['', '', 'Total', `$${totalMonto.toFixed(2)}`]);
+
+    // Crear la tabla usando autoTable
+    doc.autoTable({
+        head: head,
+        body: body,
+        startY: 40 // Iniciar la tabla después del título y la imagen
+    });
+
+    // Guardar el PDF
+    doc.save(`COMPRAS_${nombreUpper}.pdf`);
+}
+
+});
+
 
         const deleteButtons = document.querySelectorAll('.deletebtn');
 
