fedb16d455db803b1c9065fa5aa820158a7a417e
David Aguilar <utm19040003@utma.edu.mx>
2024-12-02 14:59:41 -0600

    Fix estatus 2 proyectosv3

--- editarproyecto.php
+++ editarproyecto.php
@@ -130,7 +130,7 @@ if (isset($_SESSION['codigo'])) {
                                                 </div>
 
                                                 <?php
-                                                if ($registro['estatus'] == 1) {
+                                                if ($registro['estatus'] == 1  || $registro['estatus'] == 2) {
                                                     if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], [1, 2])) {
                                                         echo '<div class="form-floating col-12 col-md-6 mt-3">
                                                         <input type="text" class="form-control" name="presupuesto" id="presupuesto" value="' . $registro['presupuesto'] . '">
@@ -172,7 +172,7 @@ if (isset($_SESSION['codigo'])) {
                                                         <select class="form-select" name="estatus" id="estatus">
                                                         <option disabled>Seleccione un estatus</option>
                                                         <option value="2" ' . ($estatus_actual == 2 ? 'selected' : '') . '>Anteproyecto</option>
-                                                        <option value="3" ' . ($estatus_actual == 3 ? 'selected' : '') . '>Rechazado</option>
+                                                        <option value="1" ' . ($estatus_actual == 1 ? 'selected' : '') . '>Rechazado</option>
                                                         </select>
                                                         <label for="estatus">Estatus</label>
                                                     </div>';
@@ -226,7 +226,7 @@ if (isset($_SESSION['codigo'])) {
                                                 
 
                                                 <?php
-                                                if ($registro['estatus'] == 1) {
+                                                if ($registro['estatus'] == 1  || $registro['estatus'] == 2) {
                                                 ?>
                                                     <div class="form-floating col-12 mt-3">
                                                         <textarea type="text" class="form-control" name="detalles" id="detalles" style="min-height:150px;"><?= $registro['detalles']; ?></textarea>
@@ -261,61 +261,6 @@ if (isset($_SESSION['codigo'])) {
             </div>
         </div>
     </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">NUEVO USUARIO</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="codeusuarios.php" method="POST">
-                        <div class="row">
-                            <div class="col-12 mtop">
-                                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre" autocomplete="off" required>
-                            </div>
-
-                            <div class="col-6 mtop">
-                                <input type="text" class="form-control" name="apellidop" id="apellidop" placeholder="Apellido paterno" autocomplete="off" required>
-                            </div>
-
-                            <div class="col-6 mtop">
-                                <input type="text" class="form-control" name="apellidom" id="apellidom" placeholder="Apellido materno" autocomplete="off" required>
-                            </div>
-
-                            <div class="col-5 mtop">
-                                <input type="text" class="form-control" name="username" id="username" placeholder="Nombre de usuario" autocomplete="off" required>
-                            </div>
-
-                            <div class="col-7 mtop">
-                                <input type="password" class="form-control" name="password" id="password" placeholder="Password" autocomplete="off" required>
-                            </div>
-
-                            <div class="col-12 mtop">
-                                <select class="form-select" name="rol" id="rol" autocomplete="off" required>
-                                    <option disabled>Categoría</option>
-                                    <option value="1">Bachillerato</option>
-                                    <option value="2">Licenciatura escolarizada</option>
-                                    <option value="3">Licenciatura ejecutiva</option>
-                                    <option value="4">Administrador</option>
-                                    <option value="5">Control escolar</option>
-                                </select>
-                            </div>
-                        </div>
-
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                    <button type="submit" class="btn btn-primary" name="save">Guardar</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
